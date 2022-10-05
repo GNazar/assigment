ad94a5a216d6d24f353eb52e257cba1a050d153f
Nazar Hrytsuniak <nazarhr@ext.inditex.com>
2022-10-05 23:20:47 +0300

    Create empty Sping project

---
+++ src/test/java/sensysgatso/com/traffic/TraficApplicationTests.java
@@ -0,0 +1,13 @@
+package sensysgatso.com.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

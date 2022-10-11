575d84eefea92e4a6aaa81bbef74b0cc9c53dc40
Nazar Hrytsuniak <nazarhr@ext.inditex.com>
2022-10-11 10:26:09 +0300

    Remove not used endpoint

--- src/main/java/sensysgatso/com/traffic/controller/EventController.java
+++ src/main/java/sensysgatso/com/traffic/controller/EventController.java
@@ -7,7 +7,6 @@ import sensysgatso.com.traffic.dto.EventDto;
 import sensysgatso.com.traffic.service.EventService;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("v1/event")
@@ -21,11 +20,4 @@ public class EventController {
     public void createEvent(@Valid @RequestBody EventDto eventDto) {
         eventService.processEvent(eventDto);
     }
-
-    //todo: for test, delete
-    @GetMapping
-    public List<EventDto> getEvents() {
-        return eventService.getEvents();
-    }
-
 }
